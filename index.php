a7e056b66bbd315a31ad8d08262577fe58fba6f2
Pejh <pejhmon@users.noreply.github.com>
2015-08-15 18:04:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+//header("Content-Type: application/json; charset=UTF-8");
 
 
 $postdata = file_get_contents("php://input");
